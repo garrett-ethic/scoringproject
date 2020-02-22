c251cadba594c3ef309baf3f6d76fc35339ac528
prestontai <35822803+prestontai@users.noreply.github.com>
2020-02-22 13:23:55 -0800

    Add files via upload
    Brandon Tom's commit

--- routes/api/calculate.js
+++ routes/api/calculate.js
@@ -1,26 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/User');
+const Shampoo = require('../../models/Shampoo');
 
 // @route   GET api/calulate
 // @desc    Return Graded user preferences when given an input form
 // @access  Public
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   try {
     // res.json({ msg: 'Test request Works!' });
 
-    const { envInput, socialInput } = req.body;
+    const { sku, userID } = req.body;
 
-    inputs = {
-      envInput,
-      socialInput
-    };
+	let product = await Shampoo.find({"sku": sku});
+	let user = await User.find({"id": userID});
+	
+	if (!product) {
+		return res.status(404).json({msg: 'Product not found'});
+	}
+	
+	if (!user) {
+		return res.status(404).json({msg: 'User not found'});
+	}
+	
+	userMetrics = user[0].metrics;
+	productMetrics = product[0].metrics;
+	
+	score = productMetrics.metric1 * userMetrics.reuse + productMetrics.metric2 * userMetrics.env + productMetrics.metric3 * userMetrics.social;
 
-    scores = {
-      envScore: 'A',
-      socialScore: 'B'
-    };
-
-    res.json(scores);
+    res.json(score);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
