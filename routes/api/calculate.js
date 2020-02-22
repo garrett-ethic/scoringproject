4cb384087656dc06a8ac1a7c436c94276f4d6e3a
Nathan <nathanjcho102@gmail.com>
2020-02-22 14:31:36 -0800

    changed find to findOne and implemented raw score

--- routes/api/calculate.js
+++ routes/api/calculate.js
@@ -12,22 +12,30 @@ router.get('/', async (req, res) => {
 
     const { sku, userID } = req.body;
 
-	let product = await Shampoo.find({"sku": sku});
-	let user = await User.find({"id": userID});
+	let product = await Shampoo.findOne({"sku": sku});
+	let user = await User.findOne({"id": userID});
 	
 	if (!product) {
 		return res.status(404).json({msg: 'Product not found'});
 	}
-	
 	if (!user) {
 		return res.status(404).json({msg: 'User not found'});
 	}
 	
-	userMetrics = user[0].metrics;
-	productMetrics = product[0].metrics;
+	// assume that every product has a metric when initialized
+	userMetrics = user.metrics;
+	productMetrics = product.metrics;
+	
+	//not needed because if the user has not submitted their prefs, score will default to 0
+	// if (!userMetrics) {
+	// 	return res.status(404).json({msg: 'User Metrics not found'});
+	// }
 	
 	score = productMetrics.metric1 * userMetrics.reuse + productMetrics.metric2 * userMetrics.env + productMetrics.metric3 * userMetrics.social;
-
+	// if score is 0, user does not have any metrics (assume for now)
+	if (score === 0) {
+		score = productMetrics.metric1 + productMetrics.metric2 + productMetrics.metric3;
+	}
     res.json(score);
   } catch (err) {
     console.error(err.message);
