9a70ff5832243931a31448ed41ef5bf9ae3bcab9
Ivan Huang <huangiw@uci.edu>
2020-02-29 22:52:42 -0800

    Added axios and url Dependencies. Started working on HTML Mockup

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
-const db = "mongodb+srv://ethicScore_dev:ethicScore_dev@ethicscore-lvey1.mongodb.net/test?retryWrites=true&w=majority"
+const db =
+  'mongodb+srv://ethicScore_dev:ethicScore_dev@ethicscore-lvey1.mongodb.net/test?retryWrites=true&w=majority';
 const app = express();
 
 // Allows us to read and parse JSON objects
@@ -16,12 +18,11 @@ const connectDB = async () => {
       useFindAndModify: false
     });
     console.log('MongoDB Connected...');
-
   } catch (err) {
     console.error(err.message);
     process.exit();
   }
-}
+};
 connectDB();
 
 const PORT = process.env.PORT || '5000';
@@ -30,9 +31,16 @@ const PORT = process.env.PORT || '5000';
 app.use('/api/calculate', require('./routes/api/calculate'));
 app.use('/api/product', require('./routes/api/product'));
 app.use('/api/user', require('./routes/api/user'));
-app.use('/api/shopifyProduct', require('./routes/api/shopifyProduct'));
+// app.use('/api/shopifyProduct', require('./routes/api/shopifyProduct'));
+
+// Serve HTML file for Mock User interface. Viewd at http://localhost:5000
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+// Serves static files for mock user interface
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
-
