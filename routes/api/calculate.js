f04a72f1843907e22a4090adde58f2f333e03749
Brandon Tom <brandontom96@gmail.com>
2020-05-15 18:27:01 -0800

    Calculate default score

--- routes/api/calculate.js
+++ routes/api/calculate.js
@@ -1,152 +1,277 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../../models/User');
-const Shampoo = require('../../models/Shampoo');
-const ShopifyProduct = require('../../models/ShopifyProduct');
+const axios = require('axios');
+//const metricweights = require('../../metric-weights.json');
 
 // EVENTUALLY, replace user with actual user
 
+const metricweights =
+{
+  "co_im": {
+    "USA_made": 2,
+    "employs_locally": 2,
+    "community_activism": 2,
+    "business_size": 2,
+    "donates_locally": 2,
+    "one_percent_for_the_planet": 2,
+    "political_donations": 2,
+    "donate_to_oppressed": 2,
+    "bcorp": 2,
+    "local_zip": 2
+  },
+  "eco_f": {
+    "sustainable_packaging": 2,
+    "sustainable_materials": 2,
+    "zeroCarbon_shipping": 2,
+    "zeroCarbon_manufacturing": 2,
+    "manufacturing_impact": 2,
+    "fsc": 2,
+    "rainforest_alliance": 2,
+    "cradle_to_cradle": 2,
+    "donate_to_environment": 2,
+    "bcorp": 2
+  },
+  "all_n": {
+    "certified_organic": 2.2222,
+    "organic_practices": 2.2222,
+    "allNatural_ingredients": 2.2222,
+    "reef_safe": 2.2222,
+    "ewg": 2.2222,
+    "madeSafe": 2.2222,
+    "consumerLabs": 2.2222,
+    "transparency": 2.2222,
+    "bcorp": 2.2222
+  },
+  "an_ri": {
+    "vegan": 6.6666,
+    "donate_to_animalRights": 6.6666,
+    "cruelty_free": 6.6666
+  },
+  "labor": {
+    "childcare": 1.4285,
+    "gym_recreation": 1.4285,
+    "educational_ops": 1.4285,
+    "healthcare": 1.4285,
+    "mobility": 1.4285,
+    "can_unionize": 1.4285,
+    "living_wage": 1.4285,
+    "safe_work_conditions": 1.4285,
+    "no_child_labor": 1.4285,
+    "empower_oppressed": 1.4285,
+    "co_op": 1.4285,
+    "ethical_materials_sourcing": 1.4285,
+    "bcorp": 1.4285,
+    "fair_trade": 1.4285
+  }
+};
+
+
+
+const shopifyAxios = axios.create({
+  baseURL:
+    "https://ethic-marketplace.myshopify.com/admin/api/2020-01/",
+  auth: {
+    username: process.env.SHOPIFY_STORE_USERNAME,
+    password: process.env.SHOPIFY_STORE_PASSWORD,
+  },
+});
+
+
 // @route   GET api/calulate
-// @desc    Return calculated ethic score given shopify product and userID
+// @desc    Return calculated default ethic score given shopify product
 // @access  Public
-router.get('/shopify/:userID/:productID', async (req, res) => {
+router.get('/:productID', async (req, res) => {
   try {
-    const { userID, productID } = req.params;
+    const productMetrics = await shopifyAxios.get('products/' + req.params.productID + '/metafields.json');
+    let defaultScore = 0;
+    let possibleScore = 0;
 
-    let user = await User.findById(userID);
+    let i;
+    let j;
 
-    let product = await ShopifyProduct.findById(productID);
+    //for (const category in productMetrics.data.metafields) {
+    for (i = 0; i < productMetrics.data.metafields.length; ++i)  {
+        const category = productMetrics.data.metafields[i];
+        if (category['key'] == 'co_im') {
+            console.log(category);
+            const co_im_values = JSON.parse(category['value']);
 
-    if (!user) {
-      return res.status(404).json({ msg: 'User not found' });
+            for (const metric in co_im_values) {
+            //for (j = 0; j < co_im_values.length; ++j) {
+                if (metric == 'business_size') {
+                    if (co_im_values[metric] == 's') {
+                        defaultScore += 2;
+                        possibleScore += 2;
+                    }
+                    else if (co_im_values[metric] == 'm') {
+                        defaultScore += 1;
+                        possibleScore += 2;
+                    }
+                    else if (co_im_values[metric] == 'l') {
+                        possibleScore += 2;
+                    }
+
+                }
+                else {
+                    if (co_im_values[metric] == 'y') {
+                        defaultScore += metricweights['co_im'][metric];
+                        possibleScore += metricweights['co_im'][metric];
+                    }
+                    else if(co_im_values[metric] == 'n') {
+                        possibleScore += metricweights['co_im'][metric];
+                    }
+                }
+            }
+
+        }
+        else if (category['key'] == 'all_n') {
+            const all_n_values = JSON.parse(category['value']);
+
+            for (const metric in all_n_values) {
+                if (metric == 'ewg' || metric == 'consumerLabs') {
+                    defaultScore += metricweights['all_n'][metric] * all_n_values[metric] / 10;
+                    possibleScore += metricweights['all_n'][metric];
+                }
+                else {
+                    if (all_n_values[metric] == 'y') {
+                        defaultScore += metricweights['all_n'][metric];
+                        possibleScore += metricweights['all_n'][metric];
+                    }
+                    else if(all_n_values[metric] == 'n') {
+                        possibleScore += metricweights['all_n'][metric];
+                    }
+                }
+            }
+        }
+        else {
+            const cat = category['key'];
+            const cat_values = JSON.parse(category['value']);
+
+            for (const metric in cat_values) {
+                if (cat_values[metric] == 'y') {
+                    defaultScore += metricweights[cat][metric];
+                    possibleScore += metricweights[cat][metric];
+
+                }
+                else if(cat_values[metric] == 'n') {
+                     possibleScore += metricweights[cat][metric];
+                }
+            }
+        }
     }
 
-    if (!product) {
-      return res.status(404).json({ msg: 'Product not found' });
-    }
+    let results = {
+        'defaultScore': defaultScore,
+        'possibleScore': possibleScore,
+        'ethicScore': defaultScore / possibleScore * 100
+    };
 
-    // assume that every product has a metric when initialized
-    userMetrics = user.metrics;
-    productMetrics = product.metrics;
+    res.json(results);
 
-    if (!userMetrics) {
-      return res.status(404).json({ msg: 'User Metrics not found' });
-    }
-
-    let score =
-      productMetrics.bcorp * userMetrics.reuse +
-      productMetrics.env * userMetrics.env +
-      productMetrics.social * userMetrics.social;
-    // if score is 0, user does not have any metrics (assume for now)
-    if (score === 0) {
-      score = productMetrics.reuse + productMetrics.env + productMetrics.social;
-    }
-    res.json({ score: score });
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
   }
 });
 
-// @route   GET api/calulate
-// @desc    Return default ethic score
-// @access  Public
-router.get('/shopify/:productID', async (req, res) => {
+/*
+router.get('/:productID/:userID', async (req, res) => {
   try {
-    const { productID } = req.params;
+    const user = await shopifyAxios.get('customers/' + req.params.userID + "/metafields.json");
+    const productMetrics = await shopifyAxios.get('products/' + req.params.productID + '/metafields.json');
+    let defaultScore = 0;
+    let possibleScore = 0;
 
-    let product = await ShopifyProduct.findById(productID);
+    let i;
+    let j;
 
-    if (!product) {
-      return res.status(404).json({ msg: 'Product not found' });
+    //for (const category in productMetrics.data.metafields) {
+    for (i = 0; i < productMetrics.data.metafields.length; ++i)  {
+        const category = productMetrics.data.metafields[i];
+        console.log(category['key']);
+        console.log(possibleScore + ' ' + defaultScore);
+        if (category['key'] == 'co_im') {
+            console.log(category);
+            const co_im_values = JSON.parse(category['value']);
+
+            for (const metric in co_im_values) {
+            //for (j = 0; j < co_im_values.length; ++j) {
+                if (metric == 'business_size') {
+                    if (co_im_values[metric] == 's') {
+                        defaultScore += 2;
+                        possibleScore += 2;
+                    }
+                    else if (co_im_values[metric] == 'm') {
+                        defaultScore += 1;
+                        possibleScore += 2;
+                    }
+                    else if (co_im_values[metric] == 'l') {
+                        possibleScore += 2;
+                    }
+
+                }
+                else {
+                    if (co_im_values[metric] == 'y') {
+                        defaultScore += metricweights['co_im'][metric];
+                        possibleScore += metricweights['co_im'][metric];
+                    }
+                    else if(co_im_values[metric] == 'n') {
+                        possibleScore += metricweights['co_im'][metric];
+                    }
+                }
+            }
+
+        }
+        else if (category['key'] == 'all_n') {
+            const all_n_values = JSON.parse(category['value']);
+
+            for (const metric in all_n_values) {
+                if (metric == 'ewg' || metric == 'consumerLabs') {
+                    defaultScore += metricweights['all_n'][metric] * all_n_values[metric] / 10;
+                    possibleScore += metricweights['all_n'][metric];
+                }
+                else {
+                    if (all_n_values[metric] == 'y') {
+                        defaultScore += metricweights['all_n'][metric];
+                        possibleScore += metricweights['all_n'][metric];
+                    }
+                    else if(all_n_values[metric] == 'n') {
+                        possibleScore += metricweights['all_n'][metric];
+                    }
+                }
+            }
+        }
+        else {
+            const cat = category['key'];
+            const cat_values = JSON.parse(category['value']);
+
+            for (const metric in cat_values) {
+                console.log('thing ' + metricweights[cat][metric]);
+                if (cat_values[metric] == 'y') {
+                    defaultScore += metricweights[cat][metric];
+                    possibleScore += metricweights[cat][metric];
+
+                }
+                else if(cat_values[metric] == 'n') {
+                     possibleScore += metricweights[cat][metric];
+                }
+            }
+        }
     }
 
-    // assume that every product has a metric when initialized
-    let productMetrics = product.metrics;
+    let results = {
+        'defaultScore': defaultScore,
+        'possibleScore': possibleScore,
+        'ethicScore': defaultScore / possibleScore * 100
+    };
 
-    let score =
-      productMetrics.bcorp * 5 +
-      productMetrics.env * 5 +
-      productMetrics.social * 5;
+    res.json(results);
 
-    res.json({ score: score });
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
   }
 });
-
-// @route   GET api/calulate
-// @desc    Return calculated ethic score when given userID and product FROM Temporary models
-// @access  Public
-router.get('/:userID/:category/:sku', async (req, res) => {
-  try {
-    let product;
-    const { category, sku, userID } = req.params;
-
-    switch (category) {
-      case 'Conditioner':
-        product = await Conditioner.findOne({ sku });
-        break;
-      case 'FaceWash':
-        product = await FaceWash.findOne({ sku });
-        break;
-      case 'Lotion':
-        product = await Lotion.findOne({ sku });
-        break;
-      case 'Shampoo':
-        product = await Shampoo.findOne({ sku });
-        break;
-      case 'SkinCare':
-        product = await SkinCare.findOne({ sku });
-        break;
-      case 'SunScreen':
-        product = await Sunscreen.findOne({ sku });
-        break;
-      default:
-        return res
-          .status(404)
-          .json({ msg: 'Product Category not yet defined in Database' });
-    }
-
-    if (!product) {
-      return res.status(404).json({ msg: 'Shampoo not found' });
-    }
-
-    let user = await User.findById(userID);
-
-    if (!product) {
-      return res.status(404).json({ msg: 'Product not found' });
-    }
-    if (!user) {
-      return res.status(404).json({ msg: 'User not found' });
-    }
-
-    // assume that every product has a metric when initialized
-    userMetrics = user.metrics;
-    productMetrics = product.metrics;
-
-    //not needed because if the user has not submitted their prefs, score will default to 0
-    // if (!userMetrics) {
-    // 	return res.status(404).json({msg: 'User Metrics not found'});
-    // }
-
-    score =
-      productMetrics.metric1 * userMetrics.reuse +
-      productMetrics.metric2 * userMetrics.env +
-      productMetrics.metric3 * userMetrics.social;
-    // if score is 0, user does not have any metrics (assume for now)
-    if (score === 0) {
-      score =
-        productMetrics.metric1 +
-        productMetrics.metric2 +
-        productMetrics.metric3;
-    }
-    res.json({ score: score });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-});
+*/
 
 module.exports = router;
